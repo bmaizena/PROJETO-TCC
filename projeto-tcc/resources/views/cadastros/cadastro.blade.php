3d275481a76e6383da353abbc77907a582a60373
Daniel Sallesse <danielsallesse@gmail.com>
2024-09-12 19:29:22 -0300

    update

--- projeto-tcc/resources/views/cadastros/cadastro.blade.php
+++ projeto-tcc/resources/views/cadastros/cadastro.blade.php
@@ -13,9 +13,11 @@
 <main class="w-3/4 bg-gradient-to-r from-blue-950 to-blue-700 grid justify-items-end rounded-2xl shadow-2xl h-min relative">
     <div class="absolute top-6 left-8 ">
         <h1 class=" font-thin text-5xl text-slate-50">BUS.IO</h1>
+        
     </div>
+    
 
-    <form action="/cadastros/recCadastro" method="POST" class=" m-1 flex flex-col bg-slate-100 p-5 rounded-2xl shadow-md w-1/2 ">
+    <form action="/cadastros/recCadastro" method="POST" class="  flex flex-col bg-slate-50 p-5 rounded-2xl shadow-md w-1/2 ">
         @csrf
         <h2 class="text-2xl font-bold font-mono text-center mb-4">Registre-se</h2>
 
@@ -43,6 +45,7 @@
         <input type="Submit" value="Cadastrar" class="border border-zinc-800 shadow-md rounded-lg p-1 m-1 cursor-pointer bg-gradient-to-r from-blue-400 to-cyan-400 hover:from-cyan-400 hover:to-blue-400  hover:text-slate-50 transition duration-300 delay-150 hover:delay-300 w-1/2 self-center" >
     </form>
 </main>
+<img src="bus.io.png" alt="bus">
 <br>
 <a href="/cadastros/" class=" shadow-md rounded-lg p-1 m-1 cursor-pointer bg-red-400 hover:bg-emerald-400 hover:text-slate-50 transition duration-300 delay-150 hover:delay-300 "> Voltar</a>
 
