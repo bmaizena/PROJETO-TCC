a41c593f679fa67d228e37aa1e40b4ab9561c7ef
Daniel Sallesse <danielsallesse@gmail.com>
2024-09-12 22:33:18 -0300

    up

--- projeto-tcc/resources/views/cadastros/cadastro.blade.php
+++ projeto-tcc/resources/views/cadastros/cadastro.blade.php
@@ -15,6 +15,9 @@
         <h1 class=" font-thin text-5xl text-slate-50">BUS.IO</h1>
         
     </div>
+    <div class="absolute top-12 left-12 ">
+        <img src="/bus.io.png" alt="bus" class="w-1/2">
+    </div>
     
 
     <form action="/cadastros/recCadastro" method="POST" class="  flex flex-col bg-slate-50 p-5 rounded-2xl shadow-md w-1/2 ">
@@ -45,7 +48,7 @@
         <input type="Submit" value="Cadastrar" class="border border-zinc-800 shadow-md rounded-lg p-1 m-1 cursor-pointer bg-gradient-to-r from-blue-400 to-cyan-400 hover:from-cyan-400 hover:to-blue-400  hover:text-slate-50 transition duration-300 delay-150 hover:delay-300 w-1/2 self-center" >
     </form>
 </main>
-<img src="bus.io.png" alt="bus">
+
 <br>
 <a href="/cadastros/" class=" shadow-md rounded-lg p-1 m-1 cursor-pointer bg-red-400 hover:bg-emerald-400 hover:text-slate-50 transition duration-300 delay-150 hover:delay-300 "> Voltar</a>
 
